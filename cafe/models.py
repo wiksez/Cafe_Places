bf60d94be880abc29d5003ec58ab65c09fddc06c
wiksez <moderviktor@gmail.com>
2024-03-21 19:19:47 +0100

    migration

--- cafe/models.py
+++ cafe/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -12,45 +13,38 @@ class Desserts(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
     price = models.FloatField(default=0)
-    composition = models.CharField()
-
-
-class WorkingHours(models.Model):
-    day_name = models.CharField(max_length=64)
-    start_of_work = models.TimeField()
-    end_of_work = models.TimeField()
-
-
-class InformationAboutCoffeeShop(models.Model):
-    adres = models.CharField(max_length=128)
-    phone_number = models.CharField(max_length=15)
-    work_schedule = models.OneToOneField(WorkingHours, on_delete=models.CASCADE)
-    district = models.CharField(max_length=128)
-    ranking = models.FloatField(default=0)
+    composition = models.CharField(max_length=256)
 
 
 class CoffeeShop(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
-    drinks = models.ForeignKey(Drinks, on_delete=models.CASCADE)
-    desserts = models.ForeignKey(Desserts, on_delete=models.CASCADE)
-    info = models.OneToOneField(InformationAboutCoffeeShop, on_delete=models.CASCADE)
+    drinks = models.ManyToManyField(Drinks)
+    desserts = models.ManyToManyField(Desserts)
+    adres = models.CharField(max_length=128)
+    phone_number = models.CharField(max_length=15)
+    district = models.CharField(max_length=128)
+    ranking = models.FloatField(default=0)
+    start_of_work = models.TimeField()
+    end_of_work = models.TimeField()
 
 
-class User(models.Model):
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    password = models.CharField(max_length=64)
-    username = models.CharField(max_length=64, unique=True)
+class Favorite(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     favourite_cafes = models.ForeignKey(CoffeeShop, on_delete=models.CASCADE)
     favourite_desserts = models.ForeignKey(Desserts, on_delete=models.CASCADE)
     favourite_drinks = models.ForeignKey(Drinks, on_delete=models.CASCADE)
 
 
 class Feedback(models.Model):
-    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    coffees = models.ManyToManyField(CoffeeShop, null=True)
-    rating = models.OneToOneField(User, on_delete=models.CASCADE)
+    coffees = models.ForeignKey(CoffeeShop, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'coffees'], name='feedback_constraints')
+        ]
 
--- cafe/views.py
+++ cafe/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+"""
+A. użytkownik nie zarejestrowany. Ma dostęp do następnych widoków:
+        1. Lista kawiarni
+        2. poszukiwanie kawiarni, drinka lub desera
+        3. Lista drinków oraz deserów w kawiarni
+        4. Czytanie opinii od innych użytkowników
+        5. Sortowanie kawiarni według rankinga, dzielnicy
+B. użytkownik zarejestrowany. Ma dodatkowo dostęp do następnych widoków:
+        1. Napisanie opinii do kawiarni
+        2. Wystawienie oceny
+        3. Może mieć własną liste ulubionych kawiarni, drinków oraz deserów
+        4. Naprawienia lub usunięcia komentaża oraz oceny
+C. 2-3 administratora mogą:
+        1. Dodawać kawiarni, drinki, dessery
+        2. Zmienać jakąkolwiek informacje w kawiarniach, meni
+        3. Usuwać kawiarni, drinki oraz dessery z listy, usuwać użytkownika, ich oceny i opinii
+"""
 
 
 def index(request):
     return HttpResponse("Głowna strona działa!")
+
