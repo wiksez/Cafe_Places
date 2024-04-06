e8413db3e0b3632eb55595b164cf5a3dccdf2891
wiksez <moderviktor@gmail.com>
2024-04-06 20:24:40 +0200

    first test

--- cafe/tests.py
+++ cafe/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+import pytest
+from cafe.models import Drinks, Desserts, CoffeeShop, Feedback
 
 # Create your tests here.
+def test_index_view():
+    client = Client()
+    url = reverse('home')
+    response = client.get(url)
+    assert response.status_code == 200
