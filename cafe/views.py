b84d783edb406926473f762bb09f36987905f3e2
wiksez <moderviktor@gmail.com>
2024-04-13 18:40:44 +0200

    documentation

--- cafe/views.py
+++ cafe/views.py
@@ -14,19 +14,19 @@ from django.contrib.auth.models import User
 """
 A. użytkownik nie zarejestrowany. Ma dostęp do następnych widoków:
         1. Lista kawiarni
-        2. poszukiwanie kawiarni, drinka lub desera
+        2. poszukiwanie drinka
         3. Lista drinków oraz deserów w kawiarni
         4. Czytanie opinii od innych użytkowników
-        5. Sortowanie kawiarni według rankinga, dzielnicy
+        5. Sortowanie kawiarni według dzielnicy
 B. użytkownik zarejestrowany. Ma dodatkowo dostęp do następnych widoków:
         1. Napisanie opinii do kawiarni
         2. Wystawienie oceny
-        3. Może mieć własną liste ulubionych kawiarni, drinków oraz deserów
-        4. Naprawienia lub usunięcia komentaża oraz oceny
-C. 2-3 administratora mogą:
-        1. Dodawać kawiarni, drinki, dessery
-        2. Zmienać jakąkolwiek informacje w kawiarniach, meni
-        3. Usuwać kawiarni, drinki oraz dessery z listy, usuwać użytkownika, ich oceny i opinii
+        3. Może mieć własną listę ulubionych kawiarni, drinka oraz desera
+        4. Lista z własnymi komentarzami na swoim profilu
+C. 1 administrator może:
+        1. Dodawać kawiarni, drinki, desery
+        2. Zmieniać jakąkolwiek informacje w kawiarniach, meni
+        3. Usuwać kawiarni, drinki oraz desery z listy
 """
 
 
